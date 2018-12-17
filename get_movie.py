919bb7aaa7d7a6950e1cb813b0b79a5383a7b907
Atul Pandey <atul.jnvs@gmail.com>
2018-12-17 20:10:18 +0530

    added support for automatically download .mkv file

--- get_movie.py
+++ get_movie.py
@@ -4,25 +4,33 @@ import time
 import requests
 import re, os
 
-movieQuery = 'Storks'
-releaseDateQuery = '23 Sep 2016'
+def match_movie_name(movieQuery, found_movie):
+	flag = True
+	for term in re.finditer(r'[a-zA-Z]+', movieQuery):
+		if term[0] not in found_movie:
+			flag = False
+	return flag
+
+def clean_movieQuery(movieQuery):
+	res = ''
+	for term in re.finditer(r'[a-zA-Z]+', movieQuery):
+		res += f'{term[0]} '
+	return res
 
 def download_movie_from_axemovies(movieQuery, releaseDateQuery, download_dir):
 	yearQuery = re.search('[0-9]{4}', releaseDateQuery)[0]
-	url = f'https://axemovies.com/?s={movieQuery.replace(" ", "+")}+{yearQuery}'
+
+	url = f'https://axemovies.com/?s={clean_movieQuery(movieQuery).replace(" ", "+")}'
 	sess = requests.session()
 	soup = BeautifulSoup(sess.get(url).content, 'lxml')
 
 	movie_links = [x.a['href'] for x in soup.find_all('div', class_='item') ]
-	print(movie_links)
-
-	download_folder = os.path.join(download_dir, f'{movieQuery} ({yearQuery}')
+	download_folder = os.path.join(download_dir, f'{clean_movieQuery(movieQuery)} ({yearQuery})')
 	profile = webdriver.FirefoxProfile()
 	profile.set_preference('browser.download.folderList', 2)
 	profile.set_preference('browser.download.manager.showWhenStarting', False)
 	profile.set_preference('browser.download.dir', download_folder)
-	profile.set_preference('browser.helperApps.neverAsk.openFile', 'text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml')
-	profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'video/x-flv, video/mp4, application/x-mpegUR, video/MP2T, video/3gpp, video/quicktime, video/x-msvideo, video/x-ms-wmv')
+	profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'video/x-flv, video/mp4, application/x-mpegUR, video/MP2T, video/3gpp, video/quicktime, video/x-msvideo, video/x-ms-wmv, video/webm, application/octet-stream' )
 	profile.set_preference('browser.helperApps.alwaysAsk.force', False)
 	profile.set_preference('browser.download.manager.alertOnEXEOpen', False)
 	profile.set_preference('browser.download.manager.focusWhenStarting', False)
@@ -37,22 +45,24 @@ def download_movie_from_axemovies(movieQuery, releaseDateQuery, download_dir):
 		data = s.find('div', class_='data')
 		quality = s.find('span', 'calidad2')
 		try:
-			if movieQuery.lower() in data.span.text.lower() \
-			and releaseDateQuery.lower() in data.span.text.lower() \
+			if match_movie_name(movieQuery.lower(), data.span.text.lower()) \
+			and yearQuery.lower() in data.span.text.lower() \
 			and (( 'hd' in quality.text.lower() and 'cam' not in quality.text.lower()) or '720p' in quality.text.lower()):
-				print(link)
+				print('Movie found!')
+				print(f'{movieQuery} {releaseDateQuery} {quality.text}')
 				browser.get(link)
 				browser.find_element_by_xpath('//input[@alt="Download Movie height="]').click()
 				result = True
 				
 				# write to the file that downloading started
-				with open('mvStatus_server.csv', 'r+') as f: # currently for local
+				with open('mvStatus_local.csv', 'r+') as f: # currently for local
 					pre_tell = 0
 					line = f.readline()
 					while len(line) != 0:
 						if line.split(',')[0] == movieQuery and yearQuery in line.split(',')[1]:
 							f.seek(pre_tell)
 							f.write(f'{line.split(",")[0]},{line.split(",")[1]},DIP\n')
+							print('Download Started')
 							break
 						pre_tell = f.tell()
 						line = f.readline()
@@ -80,6 +90,9 @@ def download_movie_from_axemovies(movieQuery, releaseDateQuery, download_dir):
 						pre_tell = f.tell()
 						line = f.readline()
 				break
+	else:
+		print(f'{movieQuery} Movie not found!')
+	browser.quit()
 	return result
 
 
@@ -87,6 +100,8 @@ with open('mvStatus_local.csv', 'r') as f:
 	for line in f:
 		if 'TBD' in line.split(',')[2]:
 			download_movie_from_axemovies(line.split(',')[0], line.split(',')[1], os.getcwd())
+
+# print(match_movie_name("Mission: Impossible - Fallout".lower(), "Mission Impossible Fallout".lower()))
 
 				
 		
